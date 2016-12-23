22b3455b5262faf04ac01e8d120c3b68ec6c0176
haochengqian11 <haochengqian11@gmail.com>
2016-12-23 18:16:54 +0800

    Change a small bug

--- app/src/main/java/cn/edu/bit/bookstore/bookstore_android/view/MainActivity.java
+++ app/src/main/java/cn/edu/bit/bookstore/bookstore_android/view/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends AppCompatActivity implements BackHandledFragme
         Intent intent = new Intent();
         intent.setClass(this,LoginActivity.class);
         startActivity(intent);
-        setTitle(R.string.navigation_example);
     }
 
     private void switchToBlog() {
