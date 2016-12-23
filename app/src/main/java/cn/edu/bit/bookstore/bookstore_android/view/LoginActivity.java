9a6caec954bc1833b2acedfcf008df06c90e7de7
haochengqian11 <haochengqian11@gmail.com>
2016-12-23 23:15:19 +0800

    add toolbar and sellBookActivity

--- app/src/main/java/cn/edu/bit/bookstore/bookstore_android/view/LoginActivity.java
+++ app/src/main/java/cn/edu/bit/bookstore/bookstore_android/view/LoginActivity.java
@@ -2,6 +2,9 @@ package cn.edu.bit.bookstore.bookstore_android.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
 import cn.edu.bit.bookstore.bookstore_android.R;
 
 
@@ -18,7 +21,17 @@ public class LoginActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.string_login);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 //        _loginButton.setOnClickListener(new View.OnClickListener() {
 //
 //            @Override
