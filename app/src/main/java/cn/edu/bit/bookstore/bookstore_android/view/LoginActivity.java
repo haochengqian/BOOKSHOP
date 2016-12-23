68dd3e2303558abd08b16a2af6d462de5bdfd3c2
satjd1996 <824909419@qq.com>
2016-12-23 11:46:10 +0800

    change handler

--- app/src/main/java/cn/edu/bit/bookstore/bookstore_android/view/LoginActivity.java
+++ app/src/main/java/cn/edu/bit/bookstore/bookstore_android/view/LoginActivity.java
@@ -6,17 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import cn.edu.bit.bookstore.bookstore_android.R;
 import cn.edu.bit.bookstore.bookstore_android.databinding.ActivityLoginBinding;
 import cn.edu.bit.bookstore.bookstore_android.model.UserLogin;
+import cn.edu.bit.bookstore.bookstore_android.viewmodel.LoginHandler;
 
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
+    private static LoginHandler handler = null;
+    private static UserLogin loginUser = null;
 
 //    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityLoginBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
-        binding.setUserLogin(new UserLogin("123","123","123"));
+
+        loginUser = new UserLogin("","","");
+        handler = new LoginHandler(loginUser);
+        binding.setUserLogin(loginUser);
+        binding.setLoginHandler(handler);
 
     }
 }
